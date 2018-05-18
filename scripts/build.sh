1db1a1ce08feb9c1bc349e5e92ebb76667456388
grgau <pedro.henrique.ferracini@gmail.com>
2018-05-18 14:05:49 -0300

    mudancas no build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,19 @@
 # First day: 14/03/2018 as 18:26:00
 
 #Março
-for d in {14..31}; do echo "Building day $(date +"$d Mar %Y")..."; python3 ../src/run.py -s "$(date "+%Y-03-"$d"T00:00:00.000Z")" -e "$(date "+%Y-03-"$d"T23:59:59.000Z")"; done &
+#for d in {15..31}; do
+#  for h in {0..22}; do
+#    hour_plus_one=$(printf %02d $(($h + 1)))
+#    hour=$(printf %02d $(($h)))
+#    echo "Building day $(date +"$d Mar %Y - $hour:00:00 to $hour_plus_one:00:00")..."; python3 ../src/run.py -s "$(date "+%Y-03-"$d"T"$hour":00:00.000Z")" -e "$(date "+%Y-03-"$d"T"$hour_plus_one":00:00.000Z")";
+#  done
+#done
 
 #Abril
-for d in {01..30}; do echo "Building day $(date +"$d Abr %Y")..."; python3 ../src/run.py -s "$(date "+%Y-04-"$d"T00:00:00.000Z")" -e "$(date "+%Y-04-"$d"T23:59:59.000Z")"; done &
+for d in {19..31}; do
+  for h in {0..22}; do
+    hour_plus_one=$(printf %02d $(($h + 1)))
+    hour=$(printf %02d $(($h)))
+    echo "Building day $(date +"$d Abr %Y - $hour:00:00 to $hour_plus_one:00:00")..."; python3 ../src/run.py -s "$(date "+%Y-04-"$d"T"$hour":00:00.000Z")" -e "$(date "+%Y-04-"$d"T"$hour_plus_one":00:00.000Z")";
+  done
+done
